e8923cff976aab5a74b4f7d6ceb197bbfed11221
JeroenVinke <jeroenvinke17@gmail.com>
2017-06-15 14:45:43 +0200

    add css processors

--- lib/resources/src/main-webpack.ts
+++ lib/resources/src/main-webpack.ts
@@ -1,7 +1,8 @@
+/// <reference types="aurelia-loader-webpack/src/webpack-hot-interface"/>
+// we want font-awesome to load as soon as possible to show the fa-spinner
 import {Aurelia} from 'aurelia-framework'
 import environment from './environment';
 import {PLATFORM} from 'aurelia-pal';
-import 'babel-polyfill';
 import * as Bluebird from 'bluebird';
 
 // remove out if you don't want a Promise polyfill (remove also from webpack.config.js)
